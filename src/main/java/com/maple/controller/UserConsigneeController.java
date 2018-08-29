27025792ac7af841069bbf6f1c3a8ab08d8bf351
GoriilA <601332575@qq.com>
2018-08-29 12:42:37 +0800

    userConsignee api

--- src/main/java/com/maple/controller/UserConsigneeController.java
+++ src/main/java/com/maple/controller/UserConsigneeController.java
@@ -1,33 +1,71 @@
 package com.maple.controller;
 
+import com.auth0.jwt.interfaces.Claim;
 import com.maple.dto.UserConsigneeDTO;
-import com.maple.entity.UserConsignee;
+import com.maple.service.UserConsigneeService;
 import com.maple.util.result.Result;
+import com.maple.util.result.ResultCode;
+import com.maple.util.result.ResultTemplate;
+import com.maple.util.token.TokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/userConsignee")
 public class UserConsigneeController {
+    @Autowired
+    private UserConsigneeService userConsigneeService;
+
     @GetMapping("/")
     public Result listUserConsignee(HttpServletRequest request) {
-        return null;
+        String token = request.getHeader("access_token");
+        Map<String, Claim> claims = TokenUtil.verifyToken(token);
+        if (null != claims) {
+            int userId = claims.get("uid").asInt();
+            return ResultTemplate.getSuccessResult(userConsigneeService.listUserConsignee(userId));
+        } else {
+            return ResultTemplate.getFreeResult(ResultCode.UNAUTHORIZED, "认证失败", null);
+        }
     }
 
     @PostMapping("/")
-    public Result saveUserConsignee(HttpServletRequest request,@RequestBody UserConsigneeDTO userConsigneeDTO) {
-        return null;
+    public Result saveUserConsignee(HttpServletRequest request, @RequestBody UserConsigneeDTO userConsigneeDTO) {
+        String token = request.getHeader("access_token");
+        Map<String, Claim> claims = TokenUtil.verifyToken(token);
+        if (null != claims) {
+            int userId = claims.get("uid").asInt();
+            userConsigneeDTO.setUserId(userId);
+            return ResultTemplate.getSuccessResult(userConsigneeService.saveUserConsignee(userConsigneeDTO));
+        } else {
+            return ResultTemplate.getFreeResult(ResultCode.UNAUTHORIZED, "认证失败", null);
+        }
     }
 
     @PatchMapping("/")
     public Result updateUserCosignee(HttpServletRequest request, @RequestBody UserConsigneeDTO userConsigneeDTO) {
-        return null;
+        String token = request.getHeader("access_token");
+        Map<String, Claim> claims = TokenUtil.verifyToken(token);
+        if (null != claims) {
+            int userId = claims.get("uid").asInt();
+            return ResultTemplate.getSuccessResult(userConsigneeService.updateUserConsignee(userConsigneeDTO, userId));
+        } else {
+            return ResultTemplate.getFreeResult(ResultCode.UNAUTHORIZED, "认证失败", null);
+        }
     }
 
-    @DeleteMapping("/")
-    public Result deleteUserConsignee(HttpServletRequest request,@RequestBody Integer id) {
-        return null;
+    @DeleteMapping("/{id}")
+    public Result deleteUserConsignee(HttpServletRequest request, @PathVariable Integer id) {
+        String token = request.getHeader("access_token");
+        Map<String, Claim> claims = TokenUtil.verifyToken(token);
+        if (null != claims) {
+            int userId = claims.get("uid").asInt();
+            return ResultTemplate.getSuccessResult(userConsigneeService.deleteUserConsignee(id, userId));
+        } else {
+            return ResultTemplate.getFreeResult(ResultCode.UNAUTHORIZED, "认证失败", null);
+        }
     }
 }
